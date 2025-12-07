41fdc98792b9a364fc429bf6cb7cc3f085b93e0a
zai <zairysbigtae@gmail.com>
2025-12-07 19:28:11 +0800

    just added println to provide details to end user

--- rust_lib/src/args_parser/quarantine/mod.rs
+++ rust_lib/src/args_parser/quarantine/mod.rs
@@ -60,8 +60,12 @@ impl Quarantinizer {
                     Ok(metadata) => metadata.permissions(),
                     Err(e) => return Err(format!("Couldn't get the metadata of `{:?}`\nError: {e}", full_quarantine_file_path)),
                 };
+
+                println!("Locking {:?}", full_quarantine_file_path);
                 perm.set_mode(0o000); // lock it. even for the user, except root can change it soo yeah
                 fs::set_permissions(full_quarantine_file_path, perm).unwrap();
+
+                println!("Locked {:?} with perms 0o000", full_quarantine_file_path);
             }
         }
 
